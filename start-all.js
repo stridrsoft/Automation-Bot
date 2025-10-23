3faf57ecb76cd2cc06372d3b0eb7dcf725aa11bb
proshielddev <proshieldlogisticsllc@gmail.com>
2025-10-23 08:04:03 +0100

    Fixes

--- start-all.js
+++ start-all.js
@@ -2,21 +2,36 @@
 const { spawn } = require('child_process');
 const path = require('path');
 
-// Start API
+console.log('Starting Automation Bot services...');
+
+// Start API on Render's PORT
 const api = spawn('npm', ['start'], { 
   cwd: path.join(__dirname, 'api'), 
   stdio: 'inherit',
-  env: { ...process.env, PORT: process.env.PORT || 4000 }
+  env: { 
+    ...process.env, 
+    PORT: process.env.PORT || 4000,
+    NODE_ENV: 'production'
+  }
 });
 
 // Start Worker  
 const worker = spawn('npm', ['start'], { 
   cwd: path.join(__dirname, 'worker'), 
-  stdio: 'inherit' 
+  stdio: 'inherit',
+  env: { ...process.env, NODE_ENV: 'production' }
 });
 
 // Handle graceful shutdown
 process.on('SIGTERM', () => {
+  console.log('Shutting down services...');
+  api.kill();
+  worker.kill();
+  process.exit(0);
+});
+
+process.on('SIGINT', () => {
+  console.log('Shutting down services...');
   api.kill();
   worker.kill();
   process.exit(0);
